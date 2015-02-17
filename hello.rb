a6df24cd135352fb1e9b78f52959f57ebfca4ff5
tommasobratto <tbratto22@gmail.com>
2015-02-17 11:41:00 +0000

    added cat picture to page

--- hello.rb
+++ hello.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
 
-get '/' do
-  'Hello, world!'
+get '/secret' do
+  'This is a secret page'
+end
+
+get '/something' do
+  'something'
+end
+
+get "/" do
+  "<div>
+    <img src=http://bit.ly/1eze8aE>
+  </div>"
 end
