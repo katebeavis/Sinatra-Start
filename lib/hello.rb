3d34f8680b4b26ae0c88c1622434527bd70cb822
Kate Beavis <katebeavis84@gmail.com>
2015-02-17 11:47:26 +0000

    Added views folder and cat name

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,6 @@
 require 'sinatra'
 
 get '/' do
-  "<div style ='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-  </div>"
+  @name = %w(Amigo Oscar Viking).sample
+  erb :index
 end
